2c048608e368747654aa376d9031adad15b9940d
limaluca <lucaslimamesmo@gmail.com>
2020-04-24 20:40:59 -0300

    Adicoes e modificacoes na pagina de cadastro

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const routes = require('./routes');//
+const routes = require('./routes');
 
 
 const server = express();
 
-
+server.use(express.urlencoded({ extended: true}))/* Habilita o acesso aos dados no req.body */
 server.use(express.static('public'));
 server.use(routes)//
 
