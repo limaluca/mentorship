fd4af58134fb9d910270cfb7d370b60c3138b85c
limaluca <lucaslimamesmo@gmail.com>
2020-04-24 20:32:51 -0300

    Mudando funcoes de manipulacao de dados para aquivo separado

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const teachers = require('./teachers')
 routes.get('/',function(req,res){
     return res.redirect("mentors")
 })
@@ -8,6 +8,12 @@ routes.get('/',function(req,res){
 routes.get("/mentors",function(req,res){
     return res.render("mentors/index")
 })
+
+routes.get("/mentors/creation",function(req,res){
+    return res.render("mentors/creation")
+})
+
+routes.post("/mentors",teachers.post)
 
 routes.get("/students",function(req,res){
     return res.render("students")
